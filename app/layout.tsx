9be8721d8aa97083c9d75449b8576883feb4c1bf
jjinendra3 <jjinendra3@gmail.com>
2025-03-30 11:58:46 +0530

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,24 @@
-import type { Metadata } from 'next'
-import './globals.css'
-import { Poppins } from 'next/font/google'
+import type { Metadata } from "next";
+import "./globals.css";
+import { Poppins } from "next/font/google";
 
 const poppins = Poppins({
-  weight: ['400', '700'],
-  subsets: ['latin'],
-})
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
-}
+  title: "MinimalForum",
+  description: "Forum For All Talks",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <body className={poppins.className}>{children}</body>
     </html>
-  )
+  );
 }
